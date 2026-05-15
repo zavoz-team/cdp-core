ca006011b3e1f024cfc9281bb34c20d7c646dc89
MoonshyXD <vladbobkb10@gmail.com>
2026-05-15 16:05:09 +0300

    [style] edit TODO coms

--- adapter/fastapi/lifespan.py
+++ adapter/fastapi/lifespan.py
@@ -7,6 +7,6 @@ from fastapi import FastAPI
 @contextlib.asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     async with contextlib.AsyncExitStack() as _stack:
-        # TODO: инициализировать и примонтировать сюда долгоживущие ресурсы (например, пул БД, HTTP-клиент)
+        # TODO: инициализировать и примонтировать сюда долгоживущие ресурсы
         # используя app.state и _stack.enter_async_context
         yield
